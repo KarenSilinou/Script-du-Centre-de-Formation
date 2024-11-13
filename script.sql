1ed8e5247849845d9cc25e19e018880227eb54a3
Karen Silinou <karensilinou@gmail.com>
2024-11-13 08:04:11 +0100

    First Commit

---
+++ script.sql
@@ -0,0 +1,66 @@
+/* Exercice 2: Centre de formation */
+
+/* 1. Creation de la base de donnees */
+CREATE DATABASE "Centre de formation";
+USE "Centre de formation";
+
+/* 2. Creation des tables */
+CREATE TABLE Etudiant (
+  numEtu INT PRIMARY KEY,
+  nomEtu VARCHAR(50) NOT NULL,
+  prenomEtu VARCHAR(50) NOT NULL,
+  dateNaissance DATE NOT NULL,
+  adresseEtu VARCHAR(100) NOT NULL,
+  villeEtu VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Session (
+  codeSession INT PRIMARY KEY,
+  dateDebut DATE NOT NULL,
+  dateFin DATE NOT NULL,
+  codeFormation INT NOT NULL,
+  CONSTRAINT CHECK_DateFin CHECK (dateFin > dateDebut),
+  FOREIGN KEY (codeFormation) REFERENCES Formation(codeForm)
+);
+
+CREATE TABLE Formation (
+  codeForm INT PRIMARY KEY,
+  intituleForm VARCHAR(100) NOT NULL,
+  dureeForm INT NOT NULL,
+  codeSess INT NOT NULL
+);
+
+CREATE TABLE Specialite (
+  codeSpec INT PRIMARY KEY,
+  intituleSpec VARCHAR(100) NOT NULL,
+  /* 5. Ajout de la colonne "Active" */
+  active TINYINT(1) NOT NULL DEFAULT 1
+);
+
+CREATE TABLE Inscription (
+  numEtu INT,
+  codeSession INT,
+  typeInscription VARCHAR(50) NOT NULL,
+  codeConcerne INT,
+  PRIMARY KEY (numEtu, codeSession),
+  FOREIGN KEY (numEtu) REFERENCES Etudiant(numEtu),
+  FOREIGN KEY (codeSession) REFERENCES Session(codeSession)
+);
+
+CREATE TABLE Concerne (
+  codeSpec INT,
+  codeForm INT,
+  PRIMARY KEY (codeSpec, codeForm),
+  FOREIGN KEY (codeSpec) REFERENCES Specialite(codeSpec),
+  FOREIGN KEY (codeForm) REFERENCES Formation(codeForm)
+);
+
+/* 3. Ajout de la contrainte NOT NULL sur la colonne typeCours de la table Inscription */
+ALTER TABLE Inscription
+ADD CONSTRAINT NOT_NULL_TypeCours
+CHECK (typeInscription IS NOT NULL);
+
+/* 4. Ajout de la contrainte CHECK dans la table SESSION */
+ALTER TABLE Session
+ADD CONSTRAINT CHECK_DateFin
+CHECK (dateFin > dateDebut);
